bfd4f23eea2de80055eaf1738fef1ec2d625a2c0
umeh emmanuel <eumeh3882@gmail.com>
2023-10-06 08:17:30 -0700

    emma

--- google/goAuth.ts
+++ google/goAuth.ts
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3678/google/callback",
+      callbackURL: "https://ajlawtesting.onrender.com/google/callback",
       // callbackURL: environemtVariable.CALLBACKURL,
       passReqToCallback: true,
     },
